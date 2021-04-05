f46f8b8f40362fc3b11b779f8bf0a16a45791ed4
RYoungJ <ruanyj3107@zju.edu.cn>
2021-04-05 10:00:53 +0800

    hessian sanity check

---
+++ sanity_check.py
@@ -0,0 +1,104 @@
+# Modified from https://github.com/deepmind/dm-haiku/blob/master/examples/transformer/model.py
+# ==============================================================================
+"""Train a transformer for language modeling on LM1B.
+This example serves to demonstrate:
+  - A clean Haiku transformer implementation.
+  - An example minimal training loop around it.
+We have not tuned the hyperparameters for LM1B at all.
+Note: Run with --alsologtostderr to see outputs.
+"""
+
+import functools
+import os
+import pickle
+import time
+from typing import Any, Mapping
+
+import haiku as hk
+import jax
+import jax.numpy as jnp
+import numpy as np
+from jax.flatten_util import ravel_pytree
+
+import optax
+import tensorflow.compat.v2 as tf
+from absl import app
+from absl import flags
+from absl import logging
+
+import sys
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/deq-jax/")
+
+from src.modules.deq import deq, wtie
+
+
+def toy_model(dim=10):
+    A = np.random.randn(dim, dim) * 0.1
+    B = np.random.randn(dim, dim)
+    b = np.random.randn(dim)
+    c = np.random.randn(dim)
+
+    params = (A, B, b, c)
+
+    data = np.random.randn(dim)
+
+    return params, data
+
+
+def analytic_fn(params, data):
+    (A, B, b, c) = params
+    dim, = b.shape
+    z_star = jnp.linalg.solve(jnp.eye(dim) - A, B @ data + b)
+    loss = c @ z_star
+
+    return loss
+
+
+def deq_fn(params, data):
+    (A, B, b, c) = params
+    dim, = b.shape
+    dummy_bias = B @ data + b
+
+    def layer(inner_params, rng, z):
+        A, = inner_params
+        z = z.squeeze((0, 1))
+        z_update = A @ z + dummy_bias
+        return jnp.expand_dims(z_update, (0, 1))
+
+    _inner_params = (A,)
+    z0 = jnp.zeros((1, 1, dim))
+    z_star = deq(_inner_params, jax.random.PRNGKey(0), z0, layer, max_iter=50, custom_vjp=False)
+    # z_star = wtie(_inner_params, jax.random.PRNGKey(0), z0, layer, feedfwd_layers=15)
+    z_star = z_star.squeeze((0, 1))
+
+    loss = c @ z_star
+
+
+    return loss
+
+
+if __name__ == '__main__':
+    np.random.seed(123)
+    params, data = toy_model()
+
+    analytic_loss = analytic_fn(params, data)
+    deq_loss = deq_fn(params, data)
+    print(analytic_loss, deq_loss)
+
+    analytic_jac = jax.jacobian(lambda p: analytic_fn(p, data))(params)
+    deq_jac = jax.jacobian(lambda p: deq_fn(p, data))(params)
+    analytic_jac, _ = ravel_pytree(analytic_jac)
+    deq_jac, _ = ravel_pytree(deq_jac)
+    # np.testing.assert_almost_equal(analytic_jac, deq_jac)
+    # print(analytic_jac, deq_jac)
+
+
+    analytic_hes = jax.hessian(lambda p: analytic_fn(p, data))(params)
+    deq_hes = jax.hessian(lambda p: deq_fn(p, data))(params)
+    analytic_hes, _ = ravel_pytree(analytic_hes)
+    deq_hes, _ = ravel_pytree(deq_hes)
+    # print(analytic_hes[:10])
+    # print(deq_hes[:10])
+
+    # np.testing.assert_almost_equal(analytic_hes, deq_hes)
