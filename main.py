30366918035cad932cf831dfbc1f0dd454e90db9
Jonah Philion <jphilion@nvidia.com>
2021-04-06 00:26:43 -0400

    add mlp

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
     Fire({
         'toy_model': source.explore.toy_model,
         'check_inverse': source.explore.check_inverse,
+
         'train_deq': source.train.train_deq,
 
         'linear_regression': source.toy.linear_regression,
